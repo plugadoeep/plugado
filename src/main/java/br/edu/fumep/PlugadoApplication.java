b77604f8623fbcf65446d78c3f8b353cb0b5582c
Raphael Basso <arabasso@host.sk>
2017-04-23 10:30:13 -0300

    Commit inicial

---
+++ src/main/java/br/edu/fumep/PlugadoApplication.java
@@ -0,0 +1,12 @@
+package br.edu.fumep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlugadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlugadoApplication.class, args);
+	}
+}
